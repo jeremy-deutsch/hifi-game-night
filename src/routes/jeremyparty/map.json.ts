398a76753c48ed3e8c9dc525b93e0655610f04b2
Jeremy Deutsch <jeremydeutsch@email.arizona.edu>
2020-09-19 14:24:07 -0700

    add CORS to map JSON

--- src/routes/jeremyparty/map.json.ts
+++ src/routes/jeremyparty/map.json.ts
@@ -159,5 +159,10 @@ export async function get(req: unknown, res: ServerResponse) {
     ],
   };
   res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   res.end(JSON.stringify(mapData));
 }
